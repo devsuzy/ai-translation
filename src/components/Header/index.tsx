4b6f973598da9055d0667511087d220353aa3263
안소현 <annsh@imform.co.kr>
2024-08-13 10:45:42 +0900

    feat:header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,35 +1,38 @@
 import Image from "next/image";
 import Link from "next/link";
+import styles from "./styles.module.scss";
 
 export default function Header() {
   return (
-    <header
-      className={`text-center w-full h-[84px] flex justify-between items-center px-[32px] py-[18px]`}
-    >
-      <Image
-        src={"/logo.svg"}
-        alt="logo"
-        width={32}
-        height={32}
-        className={`desktop:w-[48px] desktop:h-[48px]`}
-      />
-      <h1>
+    <header className={styles.header}>
+      <Link href={"/"}>
+        <Image
+          src={"/logo.svg"}
+          alt="logo"
+          width={32}
+          height={32}
+          className={`lg:w-[48px] lg:h-[48px]`}
+        />
+      </Link>
+      <h1 className={styles.copy}>
         <span className="pc-only">Smart </span>AI Translator
         <span className="pc-only"> Powered</span> by ChatGPT
       </h1>
-      <ul className={`flex items-center`}>
-        <li>
-          <Link href={"/word"}>
-            단어<span className="pc-only">번역</span>
-          </Link>
-        </li>
-        <li className={`w-[4px] h-[4px] bg-[#d9d9d9] mx-[16px]`}></li>
-        <li>
-          <Link href={"/excel"}>
-            문서<span className="pc-only">번역</span>
-          </Link>
-        </li>
-      </ul>
+      <nav>
+        <ul className={`flex items-center`}>
+          <li>
+            <Link href={"/word"}>
+              단어<span className="pc-only">번역</span>
+            </Link>
+          </li>
+          <li className={`w-[4px] h-[4px] bg-[#d9d9d9] mx-[16px]`}></li>
+          <li>
+            <Link href={"/excel"}>
+              문서<span className="pc-only">번역</span>
+            </Link>
+          </li>
+        </ul>
+      </nav>
     </header>
   );
 }
